000a13338785fa76d16bb6eec1a87151d496989a
chiefoleka <valentinenwakibeya@gmail.com>
2018-07-12 15:27:21 +0100

    Created model for admin users

---
+++ app/Admin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
