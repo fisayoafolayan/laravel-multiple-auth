db20a0cfd8b3b31c84cd368aefe67f9d57a6c7de
chiefoleka <valentinenwakibeya@gmail.com>
2018-07-12 16:29:34 +0100

    Edited the home file created by scaffolding

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.auth')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    Hi there regular user
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
