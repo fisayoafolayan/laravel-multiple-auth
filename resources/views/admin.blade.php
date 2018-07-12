0a3868143378d00d28ddac77128800917da23e78
chiefoleka <valentinenwakibeya@gmail.com>
2018-07-12 16:27:49 +0100

    admin view

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.auth')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    Hi boss!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
