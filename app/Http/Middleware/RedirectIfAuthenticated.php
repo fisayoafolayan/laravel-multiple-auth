5340ebc525ed75e33b5f73a1b7c028feba5409ea
chiefoleka <valentinenwakibeya@gmail.com>
2018-07-12 16:25:13 +0100

    redirect logged in users based on their guard

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if ($guard == "admin" && Auth::guard($guard)->check()) {
+            return redirect('/admin');
+        }
+        if ($guard == "writer" && Auth::guard($guard)->check()) {
+            return redirect('/writer');
+        }
+        if (Auth::guard($guard)->check()) {
+            return redirect('/home');
+        }
+
+        return $next($request);
+    }
+}
