a318e695ee37c2c891d059e39840c63c0a2c5f58
chiefoleka <valentinenwakibeya@gmail.com>
2018-07-12 16:40:11 +0100

    routes for the application

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/login/admin', 'Auth\LoginController@showAdminLoginForm');
+Route::get('/login/writer', 'Auth\LoginController@showWriterLoginForm');
+Route::get('/register/admin', 'Auth\RegisterController@showAdminRegisterForm');
+Route::get('/register/writer', 'Auth\RegisterController@showWriterRegisterForm');
+
+Route::post('/login/admin', 'Auth\LoginController@adminLogin');
+Route::post('/login/writer', 'Auth\LoginController@writerLogin');
+Route::post('/register/admin', 'Auth\RegisterController@createAdmin');
+Route::post('/register/writer', 'Auth\RegisterController@createWriter');
+
+Route::view('/home', 'home')->middleware('auth');
+Route::view('/admin', 'admin')->middleware('auth:admin');
+Route::view('/writer', 'writer')->middleware('auth:writer');
